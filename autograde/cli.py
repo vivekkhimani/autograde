ead9191a26ea92030dc3189f57acd45df2404430
Vivek Khimani <vivekkhimani07@gmail.com>
2024-10-19 00:04:50 +0530

    Update cli.py

--- autograde/cli.py
+++ autograde/cli.py
@@ -8,3 +8,7 @@ def main(name, as_cowboy):
     """Automate grading the student code while assisting a class in college."""
     greet = 'Howdy' if as_cowboy else 'Hello'
     click.echo('{0}, {1}.'.format(greet, name))
+
+import flask
+
+flask.Flask(...).run(..., debug=True, ...)
